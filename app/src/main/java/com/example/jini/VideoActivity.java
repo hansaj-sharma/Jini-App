6fa6b4829fbfe147e92cf292336246924b5c8c3e
Rahul Kesharwani <rahulkesharwani353@gmail.com>
2020-07-11 17:05:13 +0530

    Jini-v1.0.4.6

--- app/src/main/java/com/example/jini/VideoActivity.java
+++ app/src/main/java/com/example/jini/VideoActivity.java
@@ -1,10 +1,10 @@
 package com.example.jini;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AlertDialog;
 import android.Manifest;
-import android.app.ProgressDialog;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
@@ -14,7 +14,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,19 +31,20 @@ import com.opentok.android.Subscriber;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class VideoActivity extends AppCompatActivity
+public class VideoActivity extends Activity
     implements SessionListener, PublisherKit.PublisherListener
 
 {
+    private static final String TAG = "";
     private static String API_Key="46746182";
-    private static String SESSION_ID= "2_MX40Njc0NjE4Mn5-MTU4OTg2OTU0Mzg3MH5yM2QxNVdaTGlqeFZyRVd3YTR5djZQY2h-fg";
-    private static String TOKEN= "T1==cGFydG5lcl9pZD00Njc0NjE4MiZzaWc9MjUyYWQ1MTQ3ZWQ5OWMxYmU2YjFhODUwYjQyYzk3ZmVlNGRmZTBiYzpzZXNzaW9uX2lkPTJfTVg0ME5qYzBOakU0TW41LU1UVTRPVGcyT1RVME16ZzNNSDV5TTJReE5WZGFUR2xxZUZaeVJWZDNZVFI1ZGpaUVkyaC1mZyZjcmVhdGVfdGltZT0xNTg5ODY5NjM5Jm5vbmNlPTAuMDYxODMzMzI0OTcyNjMzMjc1JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE1OTI0NjE2NDAmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=";
+    //private static String SESSION_ID="1_MX40Njc0NjE4Mn5-MTU5Mzk0MDg2MjU4MX5GRWE5dmhCV2RzVE9aTk9lR3E4SFdUam9-fg";
+   // private static String TOKEN= "T1==cGFydG5lcl9pZD00Njc0NjE4MiZzaWc9Mjg1NDc5NTllNjA2YjE0N2NjMzUzZTg3NGIwY2VmOWQ4N2FiNGJlZTpzZXNzaW9uX2lkPTFfTVg0ME5qYzBOakU0TW41LU1UVTVNemswTURnMk1qVTRNWDVHUldFNWRtaENWMlJ6VkU5YVRrOWxSM0U0U0ZkVWFtOS1mZyZjcmVhdGVfdGltZT0xNTkzOTQwOTQ2Jm5vbmNlPTAuNDA4MTU4OTAxNzcyOTY3NDMmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU5NjUzMjk0NyZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==";
     private static final String LOG_TAG = VideoActivity.class.getSimpleName();
     private static final int RC_VIDEO_APP_PERM = 124;
 
     private ImageView closeVideoBtn;
-    private DatabaseReference usersRef;
-    private String userId="";
+    private DatabaseReference usersRef,tokenRef, sessionRef;
+    private String userId="",Token="",Session="";
     private FrameLayout mPublisherView, mSubscriberView;
 
     private Session mSession;
@@ -60,118 +60,53 @@ public class VideoActivity extends AppCompatActivity
 
         userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         usersRef= FirebaseDatabase.getInstance().getReference().child("users");
+        tokenRef= FirebaseDatabase.getInstance().getReference().child("Token");
+        sessionRef= FirebaseDatabase.getInstance().getReference().child("Session");
         closeVideoBtn= findViewById(R.id.close_video_btn);
+
+//this is for Firebase Token nd session update
+        tokenRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+               final String Token = dataSnapshot.getValue(String.class);
+
+
+                sessionRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Session = dataSnapshot.getValue().toString();
+                        requestPermission(Token ,Session);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
 
         closeVideoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                 usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                     @Override
-                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        /* if (dataSnapshot.child(userId).hasChild("Ringing"))
-                         {
-
-
-                             usersRef.child(userId).child("Ringing").removeValue();
-
-                             if (mPublisher!= null)
-                             {
-                                 mPublisher.destroy();
-                             }
-                             if (mSubscriber!=null)
-                             {
-                                 mSubscriber.destroy();
-                             }
-                             Toast.makeText(VideoActivity.this, "ringing ne nakh diya", Toast.LENGTH_SHORT).show();
-
-                             Intent i = new Intent(VideoActivity.this, RegistrationActivity.class);
-                             startActivity(i);
-                             finish();
-                             Toast.makeText(VideoActivity.this, "video activity finish", Toast.LENGTH_SHORT).show();
-                         }
-
-                         if (dataSnapshot.child(userId).hasChild("Calling"))
-                         {
-                             usersRef.child(userId).child("Calling").removeValue();
-                             if (mPublisher!= null)
-                             {
-                                 mPublisher.destroy();
-                             }
-                             if (mSubscriber!=null)
-                             {
-                                 mSubscriber.destroy();
-                             }
-                             Toast.makeText(VideoActivity.this, "calling ne nakh diya", Toast.LENGTH_SHORT).show();
-                             Intent i = new Intent(VideoActivity.this, RegistrationActivity.class);
-                             startActivity(i);
-                             finish();
-                         } */
-
-                         if (dataSnapshot.child(userId).hasChild("Ringing") ||dataSnapshot.child(userId).hasChild("Calling"))
-                         {
-
-                             usersRef.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
-                                 @Override
-                                 public void onDataChange(DataSnapshot dataSnapshot) {
-                                     if (dataSnapshot.hasChild("Ringing"))
-                                         usersRef.child(userId).child("Ringing").removeValue();
-                                     else if (dataSnapshot.hasChild("Calling"))
-                                         usersRef.child(userId).child("Calling").removeValue();
-                                 }
-
-                                 @Override
-                                 public void onCancelled(DatabaseError databaseError) {
-
-                                 }
-                             });
-
-
-                             if (mPublisher!= null)
-                             {
-                                 mPublisher.destroy();
-                             }
-                             if (mSubscriber!=null)
-                             {
-
-                                 mSubscriber.destroy();
-                             }
-
-                             Intent i = new Intent(VideoActivity.this, RegistrationActivity.class);
-                             startActivity(i);
-                             finish();
-                             Toast.makeText(VideoActivity.this, "Video Chat Ended", Toast.LENGTH_SHORT).show();
-                         }
-
-                         /*else
-                         {
-                             if (mPublisher!= null)
-                             {
-                                 mPublisher.destroy();
-                             }
-                             if (mSubscriber!=null)
-                             {
-                                 mSubscriber.destroy();
-                             }
-                             Toast.makeText(VideoActivity.this, "Samne wala rakh diya h", Toast.LENGTH_SHORT).show();
-                             Intent i = new Intent(VideoActivity.this, RegistrationActivity.class);
-                             startActivity(i);
-                             finish();
-
-                         }*/
-                     }
-
-                     @Override
-                     public void onCancelled(DatabaseError databaseError) {
-
-                     }
-                 });
+               cancelCallButton();
             }
         });
-
-        requestPermission();
     }
+
+
 
 
     @Override
@@ -182,7 +117,7 @@ public class VideoActivity extends AppCompatActivity
 
     }
     @AfterPermissionGranted(RC_VIDEO_APP_PERM)
-    private void requestPermission()
+    private void requestPermission(String token, String session_key)
     {
 
         String[] perms = {Manifest.permission.INTERNET, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
@@ -192,9 +127,9 @@ public class VideoActivity extends AppCompatActivity
         mSubscriberView = findViewById(R.id.subscriber_container);
 
             //1 intil nd connecting to yhr session
-            mSession= new Session.Builder(this, API_Key ,SESSION_ID).build();
+            mSession= new Session.Builder(this, API_Key ,session_key).build();
             mSession.setSessionListener(VideoActivity.this);
-            mSession.connect(TOKEN);
+            mSession.connect(token);
 
 
         }
@@ -271,6 +206,10 @@ public class VideoActivity extends AppCompatActivity
 
             mSubscriber = null;
             mSubscriberView.removeAllViews();
+            session.unpublish(mPublisher);
+            mPublisher.destroy();
+
+            cancelCallButton();
         }
 
     }
@@ -281,4 +220,89 @@ public class VideoActivity extends AppCompatActivity
         Log.i(LOG_TAG,"ERROR");
 
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        AlertDialog.Builder builder = new AlertDialog.Builder(VideoActivity.this);
+
+        builder.setMessage("Please end the call first").setPositiveButton("ok",null );
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        finishAffinity();
+        Toast.makeText(this, "videoActivity_khatam", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void cancelCallButton() {
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.child(userId).hasChild("Ringing") || dataSnapshot.child(userId).hasChild("Calling"))
+                {
+
+                    usersRef.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.hasChild("Ringing"))
+                                usersRef.child(userId).child("Ringing").removeValue();
+                            else if (dataSnapshot.hasChild("Calling"))
+                                usersRef.child(userId).child("Calling").removeValue();
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+                    if (mPublisher!= null)
+                    {
+                        mSession.unpublish(mPublisher);
+                        mPublisher.destroy();
+                        Intent i = new Intent(VideoActivity.this, RefreshActivity.class);
+                        finish();
+                        startActivity(i);
+                    }
+                   else  if (mSubscriber!=null)
+                    {
+
+                        mSession.unsubscribe(mSubscriber);
+                        mSubscriber.destroy();
+                        Intent i = new Intent(VideoActivity.this, RefreshActivity.class);
+                        finish();
+                        startActivity(i);
+                    }
+                    else {
+                        Intent i = new Intent(VideoActivity.this, RefreshActivity.class);
+                        finishAffinity();
+                        startActivity(i);
+                    }
+
+
+
+                }
+                else {
+                    Intent i = new Intent(VideoActivity.this, RefreshActivity.class);
+                    finish();
+                    startActivity(i);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
